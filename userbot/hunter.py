1577dc726161c28e9abbfedbfd94f9650d3f478f
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-07-15 22:37:32 +0530

    Create hunter.py

---
+++ userbot/hunter.py
@@ -0,0 +1,15 @@
+import os
+from userbot import ALIVE_NAME 
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "HUNTERBOT"
+NAME = os.environ.get("BOT_NICK_NAME")
+BOT =  = str(NAME) if NAME else "HUNTERBOT"
+#op for DEAR COMRADE
+
+Keinshin = "[DEAR COMRADE](https://t.me/Dear_comradee)"
+OP = "[HUNTERBOT](https://github.com/ComradeDear/HUNTERBOT)"
+OKAY = "[SUPPORT GROUP](https://t.me/hunterbot_support)"
+#itna test h aur aage krte h
+#test successful raha ab aage 
+ALIVE = "HUNTERBOTBOT IS ON 🔥 FIRE 🔥" 
+USERBOT = " HELLO MASTER MY NAME IS HUNTERBOT I AM A BEST USERBOT 💝"
+EMOJI = "⚡"
