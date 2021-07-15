266fa0c7e538ac1d0493826a1fbb954700f4b95c
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-07-16 01:39:46 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -26,14 +26,6 @@ sed = logging.getLogger("WARNING")
 sedprint = logging.getLogger("WARNING")
 CMD_HNDLR = Config.CMD_HNDLR
 
-if var.STRING_SESSION:
-    session_name = str(Var.STRING_SESSION)
-    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
-else:
-    session_name = "startup"
-    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
-
-
 
 INT_PLUG = ""
 LOAD_PLUG = {}
