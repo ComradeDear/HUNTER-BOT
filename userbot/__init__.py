4f1597ab46a7789a979fb460126ce980ca0b687a
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-07-16 01:33:47 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -16,7 +16,6 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 
 from userbot.thunderconfig import Config
-from var import Var
 
 from .function import thunderfunction as topfunc
 
